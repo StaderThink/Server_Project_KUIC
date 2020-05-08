ef2e8a08b74a76987c3f61b3baeede37aa94a0d5
MiNeaLoPega <jsvargas915@misena.edu>
2020-05-08 17:50:09 -0500

    Finalizar modelos de pedido y cliente

---
+++ Centaurus/Modelo/Estado.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Centaurus.Modelo
+{
+	public sealed class DetallePedido : IEntidad
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public int Orden { get; set; }
+		public bool Cancelable { get; set; }
+	}
+}
---
+++ Centaurus/Modelo/Pedido.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Centaurus.Modelo
+{
+	public sealed class Pedido : IEntidad
+	{
+		public int Id { get; set; }
+		public DateTime Fecha { get; set; }
+		public int Cliente { get; set; }
+		public int Asesor { get; set; }
+		public int Estado { get; set; }
+		public bool Cancelado { get; set; }
+		public double Descuento { get; set; }
+		public string Observacion { get; set; }
+    }
+}
