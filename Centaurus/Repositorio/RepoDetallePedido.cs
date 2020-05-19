51c8cae5b23b1814111d84e9c06c63d74f03075f
MiNeaLoPega <jsvargas915@misena.edu>
2020-05-18 19:57:25 -0500

    Agregar Repositorios de Clientes y Pedidos

---
+++ Centaurus/Repositorio/RepoDetallePedido.cs
@@ -0,0 +1,53 @@
+﻿using Centaurus.Modelo;
+using System.Collections.Generic;
+
+namespace Centaurus.Repositorio
+{
+    public sealed class RepoDetallePedido : IRepo<DetallePedido>
+    {
+        public bool Editar(DetallePedido entidad)
+        {
+            using var conexion = new Conexion();
+
+            var consulta = $@"
+                update detalle_pedido set 
+                    pedido = @Pedido,
+                    cantidad = @Cantidad,
+                    producto = @Producto
+
+                    where id = @Id
+           ";
+            var filasAfectadas = conexion.Ejecutar(consulta, entidad);
+            return filasAfectadas > 0;
+        }
+        public bool Eliminar(DetallePedido entidad)
+        {
+            using var conexion = new Conexion();
+
+            var consulta = "delete from detalle_pedido where id = @Id";
+            var filasAfectadas = conexion.Ejecutar(consulta, entidad);
+            return filasAfectadas > 0;
+        }
+        public bool Insertar(DetallePedido entidad)
+        {
+            using var conexion = new Conexion();
+            var consulta = $@"insert into (pedido, cantidad, producto) 
+                            values (@Pedido, @Cantidad, @Producto)";
+            var filasAfectadas = conexion.Ejecutar(consulta, entidad);
+            return filasAfectadas > 0;
+        }
+        public IEnumerable<DetallePedido> Listar()
+        {
+            using var conexion = new Conexion();
+            return conexion.Seleccionar<DetallePedido>("select * from detalle_pedido");
+        }
+
+        public DetallePedido PorId(int id)
+        {
+            using var conexion = new Conexion();
+            var consulta = "select * from detalle_pedido where id = @id";
+
+            return conexion.Obtener<DetallePedido>(consulta, new { id });
+        }
+    }
+}
