c10229839488ccfa99c3700eeec0b7c4965a454e
lorenacuadros <alcuaddros@misena.edu.co>
2020-07-17 21:06:51 -0500

    solventar problema en registrar producto (#7)

--- Dominio/Productos/Categoria.cs
+++ Dominio/Productos/Categoria.cs
@@ -1,8 +1,13 @@
-﻿namespace Dominio.Productos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Productos
 {
     public sealed class Categoria
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
     }
--- Dominio/Productos/Producto.cs
+++ Dominio/Productos/Producto.cs
@@ -1,18 +1,42 @@
-﻿namespace Dominio.Productos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dominio.Productos
 {
     public sealed class Producto
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
+
+        [StringLength(5, ErrorMessage = "El codigo debe tener {1} carateres")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
         public string Codigo { get; set; }
-        public double Precio { get; set; }
-        public int Existencias { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
+        public double Precio { get; set; } = 50;
+
+        [Required]
+        public int Existencias { get; set; } = 0;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
         public string MinCantidad { get; set; }
-        public double MinPeso { get; set; }
-        public double MaxPeso { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
+        public double MinPeso { get; set; } = 1;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este campo es obligatorio")]
+        public double MaxPeso { get; set; } = 0;
+
+        [Required]
         public Magnitud Magnitud { get; set; } = Magnitud.Kg;
+
+        [Required]
         public Presentacion Presentacion { get; set; } = Presentacion.Bandeja;
+
+        [Required(ErrorMessage = "Elige una categoria")]
         public int Categoria { get; set; }
     }
 }
